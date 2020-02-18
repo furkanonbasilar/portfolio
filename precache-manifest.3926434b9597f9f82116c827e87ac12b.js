ac280f8e4f14a171ecfa4704cfd7cb0ed0ac74c4
Furkan <furkan_onbasilar@hotmail.com>
2020-02-18 23:01:51 +0300

    Updates

---
+++ precache-manifest.3926434b9597f9f82116c827e87ac12b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdce11b2e962dc6867923b5980412d99",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1e21076186e8634902df",
+    "url": "/portfolio/static/css/main.4133a2b1.chunk.css"
+  },
+  {
+    "revision": "96b11594c88a12e6b11b",
+    "url": "/portfolio/static/js/2.16c3c02c.chunk.js"
+  },
+  {
+    "revision": "4a472100326949405f72d022284f4f42",
+    "url": "/portfolio/static/js/2.16c3c02c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e21076186e8634902df",
+    "url": "/portfolio/static/js/main.03751369.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/portfolio/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/portfolio/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "edda74f3c525bccc9d22f22a51270a13",
+    "url": "/portfolio/static/media/dotabuff.edda74f3.png"
+  },
+  {
+    "revision": "09679d99d3933265a228b7f8187a41dd",
+    "url": "/portfolio/static/media/ecommerce.09679d99.png"
+  },
+  {
+    "revision": "00fa55710f5b0622dfcb04a817ce21bf",
+    "url": "/portfolio/static/media/ecommerce2.00fa5571.png"
+  },
+  {
+    "revision": "3f80239c8dbd3ed6858e2b41a579b187",
+    "url": "/portfolio/static/media/furkan1.3f80239c.jpg"
+  },
+  {
+    "revision": "34bed04a69508ec33b1eddf875042b73",
+    "url": "/portfolio/static/media/furkan2.34bed04a.jpeg"
+  },
+  {
+    "revision": "b91e667116ca67abceabba12910aa6be",
+    "url": "/portfolio/static/media/furkan3.b91e6671.jpeg"
+  }
+]);
